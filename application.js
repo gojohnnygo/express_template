bd134fbc62ac4ea0d23303d2316614bca330348c
Andrew Grieser <agrieser@gmail.com>
2013-01-06 15:54:11 -0800

    Fix middleware ordering

--- application.js
+++ application.js
@@ -1,9 +1,12 @@
-var path        = require("path");
-var http        = require("http");
-var express     = require("express");
-var config      = require(path.join(__dirname, "config", "application"));
-var routes      = require(path.join(config.root, "application", "routes"));
-var CacheBuster = require(path.join(config.root, "application", "lib", "cache_buster"));
+var path         = require("path");
+var http         = require("http");
+var express      = require("express");
+var redis        = require(path.join(config.root, "application", "lib", "redis"));
+var config       = require(path.join(__dirname, "config", "application"));
+var routes       = require(path.join(config.root, "application", "routes"));
+var csrf         = require(path.join(config.root, "application", "lib", "csrf"));
+var SessionStore = require(path.join(config.root, "application", "lib", "session_store"));
+var CacheBuster  = require(path.join(config.root, "application", "lib", "cache_buster"));
 
 var app = express();
 
@@ -16,8 +19,8 @@ app.set("views", path.join(config.root, "application", "views"));
 app.set("view engine", "jade");
 app.locals.pretty = true;
 
-// load routes
-routes(app);
+// Initialize session
+config.session.store = new SessionStore({ redis: redis });
 
 // register middleware
 app.use(express.logger("tiny"));
@@ -26,6 +29,7 @@ app.use(express.cookieParser(config.session.secret));
 app.use(express.session(config.session));
 app.use(express.bodyParser());
 app.use(express.csrf());
+app.use(csrf);
 app.use(app.router);
 app.use(CacheBuster(app));
 
@@ -37,6 +41,9 @@ if (config.environment !== "production") {
 app.use(express.static(path.join(config.root, "public")));
 app.use(express.compress());
 app.use(express.errorHandler());
+
+// load routes
+routes(app);
 
 http.createServer(app).listen(config.port, function(error) {
 	if (error) {
