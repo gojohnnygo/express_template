a255bf3b622582d8d1516357b9301f480f3e5dde
Andrew Grieser <agrieser@gmail.com>
2013-01-12 18:48:17 -0800

    Add mongo use example

--- application/controllers/posts.js
+++ application/controllers/posts.js
@@ -7,14 +7,14 @@ var actions = {};
 actions.index = function(request, response) {
 	Post.all(function(error, posts) {
 		if (error) { return response.send(500); }
-		response.render("posts/index", { posts: posts} );
+		response.render("posts/index", { posts: posts.map(function(post) { return post.toJSON(); })});
 	});
 };
 
 actions.show = function(request, response) {
 	Post.find(request.params.id, function(error, post) {
 		if (error) { return response.send(404); }
-		response.render("posts/show", { post: post });
+		response.render("posts/show", { post: post.toJSON() });
 	});
 };
 
--- application/models/post.js
+++ application/models/post.js
@@ -1,15 +1,48 @@
-var Post = function() { };
+var path   = require("path");
+var _      = require("lodash");
+var config = require(path.join(__dirname, "..", "..", "config", "application"));
+var mongo  = require(path.join(config.root, "application", "lib", "mongo"));
+
+var Post = function(options) {
+	_.extend(this, options);
+};
+
+Post.collectionName = "posts";
 
 Post.all = function(callback) {
-	callback(null, [
-		{ id: 1, title: "foo", body: "body 1" },
-		{ id: 2, title: "bar", body: "body 2" },
-		{ id: 3, title: "baz", body: "body 3" }
-	]);
+	mongo.collection(this.collectionName, function(error, collection) {
+		if (error) { return callback(error); }
+		collection.find({}).limit(25).toArray(function(error, docs) {
+			if (error) { return callback(error); }
+			// fake data
+			docs = [
+				{ id: 1, title: "foo", body: "body 1" },
+				{ id: 2, title: "bar", body: "body 2" },
+				{ id: 3, title: "baz", body: "body 3" }
+			];
+			return callback(null, docs.map(function(doc) { return new Post(doc); }));
+		});
+	});
 };
 
 Post.find = function(id, callback) {
-	callback(null, {id: id, title: "foo", body: "body"});
+	mongo.collection(this.collectionName, function(error, collection) {
+		if (error) { return callback(error); }
+		collection.find({ id: id }).limit(1).nextObject(function(error, doc) {
+			if (error) { return callback(error); }
+			// fake data
+			doc = { id: id, title: "foo", body: "body" };
+			return callback(null, new Post(doc));
+		});
+	});
+};
+
+Post.prototype.toJSON = function() {
+	return {
+		id: this.id,
+		title: this.title,
+		body: this.body
+	};
 };
 
 module.exports = Post;
